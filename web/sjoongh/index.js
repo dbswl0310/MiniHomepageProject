836558ab035728215ee7a413be2023423c7e8084
Quence1151 <dlgksdyd1151@naver.com>
2022-03-21 19:58:58 +0900

    fix: 3/18

---
+++ web/sjoongh/index.js
@@ -0,0 +1,77 @@
+const apiUrl = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1';
+const IMGPATH = "https://image.tmdb.org/t/p/w1280";
+const SEARCHAPI =
+    "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
+
+const main = document.getElementById("main");
+const form = document.getElementById("form");
+const search = document.getElementById("search");
+const page1 = document.querySelector(".movie-card");
+
+function showMovies(url){
+    fetch(url).then(res => res.json())
+        .then(function(data){
+            console.log(data.results);
+            data.results.forEach(element => {
+                const el = document.createElement('div');
+                const image = document.createElement('img');
+                image.className = 'test';
+                let test = document.createTextNode('Test');
+                image.appendChild(test);
+                const text = document.createElement('h2');
+
+                text.innerHTML = `${element.title}`;
+                image.src = IMGPATH + element.poster_path;
+                el.appendChild(image);
+                el.appendChild(text);
+                main.appendChild(el);
+            });
+        });
+}
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    main.innerHTML = '';
+    page1.remove();
+    showMovies(apiUrl);
+    const searchTerm = search.value;
+    if (searchTerm) {
+        showMovies(SEARCHAPI + searchTerm);
+        search.value = "";
+    }
+});
+
+let dropdowns = document.querySelectorAll('.navbar .dropdown-toggler')
+let dropdownIsOpen = false
+
+if (dropdowns.length) {
+    dropdowns.forEach((dropdown) => {
+        dropdown.addEventListener('click', (event) => {
+            let target = document.querySelector(`#${event.target.dataset.dropdown}`)
+
+            if (target) {
+                if (target.classList.contains('show')) {
+                    target.classList.remove('show')
+                    dropdownIsOpen = false
+                } else {
+                    target.classList.add('show')
+                    dropdownIsOpen = true
+                }
+            }
+        })
+    })
+}
+
+function handleSmallScreens() {
+    document.querySelector('.navbar-toggler')
+        .addEventListener('click', () => {
+            let navbarMenu = document.querySelector('.navbar-menu');
+            if (navbarMenu.style.display === 'flex') {
+                navbarMenu.style.display = 'none'
+                return
+            }
+
+            navbarMenu.style.display = 'flex'
+        })
+}
+handleSmallScreens()
